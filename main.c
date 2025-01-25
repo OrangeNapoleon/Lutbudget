9ec072771eafe49bbfa30b0cf4c55c0512a4c1c3
OrangeNapoleon <lutpoint@gmail.com>
2025-01-25 01:13:51 +0000

    Added I/O

--- main.c
+++ main.c
@@ -1,14 +1,42 @@
 #include <stdio.h>
+#include <string.h>
 
 #define ALLOWANCE 300
 #define CONVERSION 0.8
 
+#define MAXLINE 20
+
+int getln(char line[], int limit);
+
 int main(void)
 {
 	float budget = 0.0;
-	while (1)
-	{
+	char input[100];
+	int f;
+	while (1){
 		putchar('>');
+		f = getln(input, MAXLINE);
+		printf("%s", input);
+		if (f)
+			break;
+		/*TODO: ADD FILE SAVING*/
 	}
 	return 0;
+}
+
+int getln(char s[], int lim)
+{
+	int c, i;
+	
+	for (i=0; i < lim-1 && (c=getchar())!=EOF && c!='\n'; ++i)
+		s[i] = c;
+	if (c == '\n'){
+		s[i] = c;
+		++i;
+		s[i] = '\0';
+		return 0;
+	}else{
+		strcpy(s, "EXIT\n");
+		return 1;
+	}
 }
